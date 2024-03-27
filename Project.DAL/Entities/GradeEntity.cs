b2c7869673014541f8971febc6d790115ef761a2
alexandermc22 <sasha30092003@gmail.com>
2024-03-27 17:43:01 +0100

    fix mistakes 2.0

--- Project.DAL/Entities/GradeEntity.cs
+++ Project.DAL/Entities/GradeEntity.cs
@@ -6,8 +6,8 @@ public class GradeEntity : IEntity
     public required double Score { get; set; }
     public string? Description { get; set; }
         
-    public required Guid IdAction { get; set; }
-    public required Guid IdStudent { get; set; }
+    public required Guid ActionId { get; set; }
+    public required Guid StudentId { get; set; }
     public required ActionEntity Action { get; set; }
     public required StudentEntity Student { get; set; }
 }
