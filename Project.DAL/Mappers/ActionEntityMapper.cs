30467a7b320138a19ea9f21497775d916ef41d8b
Timur1326 <nurtdinov_t13@mail.ru>
2024-03-29 18:21:52 +0100

    add mappers

---
+++ Project.DAL/Mappers/ActionEntityMapper.cs
@@ -0,0 +1,15 @@
+﻿using Project.DAL.Entities;
+
+namespace Project.DAL.Mappers;
+
+public class ActionEntityMapper : IEntityMapper<ActionEntity>
+{
+    public void MapToExistingEntity(ActionEntity existingEntity, ActionEntity newEntity)
+    {
+        existingEntity.Start = newEntity.Start;
+        existingEntity.End = newEntity.End;
+        existingEntity.LectureRoom = existingEntity.LectureRoom;
+        existingEntity.Tag = newEntity.Tag;
+        existingEntity.Description = newEntity.Description;
+    }
+}
---
+++ Project.DAL/Mappers/IEntityMapper.cs
@@ -0,0 +1,9 @@
+﻿using Project.DAL.Entities;
+
+namespace Project.DAL.Mappers;
+
+public interface IEntityMapper<in TEntyty>
+    where TEntyty : IEntity
+{
+    void MapToExistingEntity(TEntyty existingEntity, TEntyty newEntity);
+}   
