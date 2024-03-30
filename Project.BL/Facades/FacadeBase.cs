4973bcda037fb709097f118584feac786bbcdb02
Alexandr Tihanschi <sasha30092003@gmail.com>
2024-03-30 20:49:39 +0100

    add Facade

---
+++ Project.BL/Facades/FacadeBase.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Reflection;
+using Project.BL.Mappers;
+using Project.BL.Models;
+using Project.DAL.Entities;
+using Project.DAL.Mappers;
+using Project.DAL.Repositories;
+using Project.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBook.BL.Facades;
+
+public abstract class
+    FacadeBase<TEntity, TListModel, TDetailModel, TEntityMapper>(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IModelMapper<TEntity, TListModel, TDetailModel> modelMapper)
+    : IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+    where TEntityMapper : IEntityMapper<TEntity>, new()
+{
+    protected readonly IModelMapper<TEntity, TListModel, TDetailModel> ModelMapper = modelMapper;
+    protected readonly IUnitOfWorkFactory UnitOfWorkFactory = unitOfWorkFactory;
+
+    protected virtual ICollection<string> IncludesNavigationPathDetail => new List<string>();
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        try
+        {
+            await uow.GetRepository<TEntity, TEntityMapper>().DeleteAsync(id).ConfigureAwait(false);
+            await uow.CommitAsync().ConfigureAwait(false);
+        }
+        catch (DbUpdateException e)
+        {
+            throw new InvalidOperationException("Entity deletion failed.", e);
+        }
+    }
+
+    public virtual async Task<TDetailModel?> GetAsync(Guid id)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+
+        IQueryable<TEntity> query = uow.GetRepository<TEntity, TEntityMapper>().Get();
+
+        foreach (string includePath in IncludesNavigationPathDetail)
+        {
+            query = query.Include(includePath);
+        }
+
+        TEntity? entity = await query.SingleOrDefaultAsync(e => e.Id == id).ConfigureAwait(false);
+
+        return entity is null
+            ? null
+            : ModelMapper.MapToDetailModel(entity);
+    }
+
+    // Always use paging in production
+    public virtual async Task<IEnumerable<TListModel>> GetAsync()
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        List<TEntity> entities = await uow
+            .GetRepository<TEntity, TEntityMapper>()
+            .Get()
+            .ToListAsync().ConfigureAwait(false);
+
+        return ModelMapper.MapToListModel(entities);
+    }
+
+    public virtual async Task<TDetailModel> SaveAsync(TDetailModel model)
+    {
+        TDetailModel result;
+
+        GuardCollectionsAreNotSet(model);
+
+        TEntity entity = ModelMapper.MapToEntity(model);
+
+        IUnitOfWork uow = UnitOfWorkFactory.Create();
+        IRepository<TEntity> repository = uow.GetRepository<TEntity, TEntityMapper>();
+
+        if (await repository.ExistsAsync(entity).ConfigureAwait(false))
+        {
+            TEntity updatedEntity = await repository.UpdateAsync(entity).ConfigureAwait(false);
+            result = ModelMapper.MapToDetailModel(updatedEntity);
+        }
+        else
+        {
+            entity.Id = Guid.NewGuid();
+            TEntity insertedEntity = repository.Insert(entity);
+            result = ModelMapper.MapToDetailModel(insertedEntity);
+        }
+
+        await uow.CommitAsync().ConfigureAwait(false);
+
+        return result;
+    }
+
+    /// <summary>
+    /// This Guard ensures that there is a clear understanding of current infrastructure limitations.
+    /// This version of BL/DAL infrastructure does not support insertion or update of adjacent entities.
+    /// WARN: Does not guard navigation properties.
+    /// </summary>
+    /// <param name="model">Model to be inserted or updated</param>
+    /// <exception cref="InvalidOperationException"></exception>
+    private static void GuardCollectionsAreNotSet(TDetailModel model)
+    {
+        IEnumerable<PropertyInfo> collectionProperties = model
+            .GetType()
+            .GetProperties()
+            .Where(i => typeof(ICollection).IsAssignableFrom(i.PropertyType));
+
+        foreach (PropertyInfo collectionProperty in collectionProperties)
+        {
+            if (collectionProperty.GetValue(model) is ICollection { Count: > 0 })
+            {
+                throw new InvalidOperationException(
+                    "Current BL and DAL infrastructure disallows insert or update of models with adjacent collections.");
+            }
+        }
+    }
+}
---
+++ Project.BL/Facades/Interfaces/IFacade.cs
@@ -0,0 +1,15 @@
+﻿using Project.BL.Models;
+using Project.DAL.Entities;
+
+namespace Project.BL.Facades;
+
+public interface IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+{
+    Task DeleteAsync(Guid id);
+    Task<TDetailModel?> GetAsync(Guid id);
+    Task<IEnumerable<TListModel>> GetAsync();
+    Task<TDetailModel> SaveAsync(TDetailModel model);
+}
