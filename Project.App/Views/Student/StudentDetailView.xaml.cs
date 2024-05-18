e3b1f9041a51571a9f465fe5beab8d789161ec28
Alexandr Tihanschi <sasha30092003@gmail.com>
2024-05-18 17:21:03 +0200

    add StudentView

---
+++ Project.App/Views/Student/StudentDetailView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.App.ViewModels;
+
+namespace Project.App.Views.Student;
+
+public partial class StudentDetailView
+{
+    public StudentDetailView(StudentDetailViewModel viewModel)
+        // : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
