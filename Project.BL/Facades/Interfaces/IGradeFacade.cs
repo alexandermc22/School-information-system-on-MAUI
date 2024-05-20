c49dc72391faf72a1022b939a70561cfeea2def8
Dzianis Pilipenka <xpilip00@vutbr.cz>
2024-05-20 15:49:00 +0200

    Fix IGradeFacade.cs

--- Project.BL/Facades/Interfaces/IGradeFacade.cs
+++ Project.BL/Facades/Interfaces/IGradeFacade.cs
@@ -1,9 +1,10 @@
 ﻿using Project.BL.Models;
+using Project.DAL.Entities;
 
 namespace Project.BL.Facades;
 
-public interface IGradeFacade
+public interface IGradeFacade : IFacade<GradeEntity, GradeListModel, GradeDetailModel>
 {
-    Task SaveAsync(GradeDetailModel model, Guid studentId);
-    Task DeleteAsync(Guid id);
+    /*Task SaveAsync(GradeDetailModel model, Guid studentId);
+    Task DeleteAsync(Guid id);*/
 }
