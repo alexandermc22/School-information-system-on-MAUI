256b448a098509e2d7a7808357508817e65fb8ce
Alexandr Tihanschi <sasha30092003@gmail.com>
2024-05-24 17:21:04 +0200

    fix bug add subject to empty student

---
+++ Project.App/Converters/ModelIsNewToIsVisibleConverter.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Maui.Converters;
+using Project.BL.Models;
+using System.Globalization;
+
+namespace Project.App.Converters;
+
+public class ModelIsNewToIsVisibleConverter : BaseConverterOneWay<ModelBase, bool>
+{
+    public override bool ConvertFrom(ModelBase value, CultureInfo? culture)
+        => value.Id == Guid.Empty;
+    
+    public override bool DefaultConvertReturnValue { get; set; } = true;
+}
