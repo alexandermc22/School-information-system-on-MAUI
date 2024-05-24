942be2db5953e53be3ac509ded208cce3e8a3943
Alexandr Tihanschi <sasha30092003@gmail.com>
2024-05-24 17:25:05 +0200

    fix bug add empty subject

--- Project.App/ViewModels/Subject/SubjectEditViewModel.cs
+++ Project.App/ViewModels/Subject/SubjectEditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.IdentityModel.Tokens;
 using Project.App.Messages;
 using Project.App.Services;
 using Project.BL.Facades;
@@ -10,6 +11,7 @@ namespace Project.App.ViewModels;
 [QueryProperty(nameof(Subject), nameof(Subject))]
 public partial class SubjectEditViewModel(
     ISubjectFacade subjectFacade,
+    IAlertService alertService,
     INavigationService navigationService,
     IMessengerService messengerService)
     : ViewModelBase(messengerService), IRecipient<SubjectEditMessage>,IRecipient<SubjectAddMessage>, IRecipient<SubjectDeleteMessage>
@@ -23,11 +25,16 @@ public partial class SubjectEditViewModel(
     [RelayCommand]
     private async Task SaveAsync()
     {
-        await subjectFacade.SaveAsync(Subject with{ Activities = default!, SubjectStudents = default!});
+        if (Subject.Code.IsNullOrEmpty() || Subject.Name.IsNullOrEmpty())
+            await alertService.DisplayAsync("Error", "You must enter name and code");
+        else
+        {
+            await subjectFacade.SaveAsync(Subject with { Activities = default!, SubjectStudents = default! });
 
-        MessengerService.Send(new SubjectEditMessage { SubjectId = Subject.Id});
+            MessengerService.Send(new SubjectEditMessage { SubjectId = Subject.Id });
 
-        navigationService.SendBackButtonPressed();
+            navigationService.SendBackButtonPressed();
+        }
     }
 
     public async void Receive(SubjectEditMessage message)
