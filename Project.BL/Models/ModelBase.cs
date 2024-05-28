e3a022d3d98b4566b656a475cec370cf39bed293
Dzianis Pilipenka <xpilip00@vutbr.cz>
2024-05-28 18:28:53 +0200

    Fix ModelBase.cs

--- Project.BL/Models/ModelBase.cs
+++ Project.BL/Models/ModelBase.cs
@@ -1,6 +1,14 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace Project.BL.Models;
 
-public abstract record ModelBase : IModel
+public abstract record ModelBase : INotifyPropertyChanged, IModel
 {
     public Guid Id { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
